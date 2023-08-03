ea3e7a9f8369eb785148fa63f1b081d93e0cc3b5
AnthonyIfy <136186728+AnthonyIfy@users.noreply.github.com>
2023-08-02 22:01:33 -0500

    Update eks-cluster.tf

--- kubernetes/eks-cluster.tf
+++ kubernetes/eks-cluster.tf
@@ -1,16 +1,16 @@
 provider "kubernetes" {
     #load_config_file = "false"
-    host = data.aws_eks_cluster.myappp-cluster.endpoint
-    token = data.aws_eks_cluster_auth.myappp-cluster.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.myappp-cluster.certificate_authority.0.data)
+    host = data.aws_eks_cluster.myapp1-cluster.endpoint
+    token = data.aws_eks_cluster_auth.myapp1-cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.myapp1-cluster.certificate_authority.0.data)
 }
 
-data "aws_eks_cluster" "myappp-cluster" {
+data "aws_eks_cluster" "myapp1-cluster" {
     name = module.eks.cluster_id
 }
 
 
-data "aws_eks_cluster_auth" "myappp-cluster" {
+data "aws_eks_cluster_auth" "myapp1-cluster" {
     name = module.eks.cluster_id
 }
 
@@ -19,15 +19,15 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "18.21.0"
 
-  cluster_name = "myappp-eks-cluster"  
+  cluster_name = "myapp1-eks-cluster"  
   cluster_version = "1.22"
 
-  subnet_ids = module.myappp-vpc.private_subnets
-  vpc_id = module.myappp-vpc.vpc_id
+  subnet_ids = module.myapp1-vpc.private_subnets
+  vpc_id = module.myapp1-vpc.vpc_id
 
   tags = {
     environment = "development"
-    application = "myappp"
+    application = "myapp1"
   }
 
   eks_managed_node_groups = {
